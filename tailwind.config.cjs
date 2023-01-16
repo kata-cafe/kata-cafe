503f19a88addc9492233a94aa1c0e9623698494d
Runyasak Chaengnaimuang <world@odds.team>
2023-01-16 12:56:23 +0700

    config: install daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#BD34FE",
+          "secondary": "#00D8FF",
+          "accent": "#1FB2A6",
+          "neutral": "#191D24",
+          "base-100": "#242424",
+          "info": "#149DCA",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ]
+  }
 };
